9bb3f775481a752849e505cd44e24906e67442c0
Elian Ortega <elian.mortega@gmail.com>
2021-10-09 00:15:56 -0600

    feat: add contact constants

---
+++ lib/constants/contact.dart
@@ -0,0 +1,9 @@
+class Contact {
+  Contact(this.firstName, this.lastName, this.email, this.phone, this.imageUrl);
+
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String phone;
+  final String imageUrl;
+}
